e89fa22d1c14abdb2036f88e65ca5d9d5ed27dbc
Spencer Smith <stix1919@hotmail.com>
2018-03-11 15:42:59 -0700

    Can add cards

--- src/App.js
+++ src/App.js
@@ -10,16 +10,32 @@ class App extends Component {
     super(props)
     this.state = {
       color: 'blue',
+      newTitle: '',
       titles: ['is one', 'is two']
     }
 
     this.testFunction = this.testFunction.bind(this)
+
+    this.enterCardTitle = this.enterCardTitle.bind(this)
+    this.addCardTitle = this.addCardTitle.bind(this)
   }
 
   testFunction() {
     axios.get('/api/testThing').then(res => {
       this.setState({color: res.data})
     })
+  }
+  enterCardTitle(e) {
+    this.setState({newTitle: e.target.value})
+  }
+  addCardTitle(newTitle, titles) {
+
+    if(this.state.newTitle.length > 1){
+      let newTitles = titles
+      newTitles.push(newTitle)
+      this.setState({titles: newTitles, newTitle: ''})
+    }
+    
   }
 
   render() {
@@ -36,6 +52,10 @@ class App extends Component {
         <div>
           {cardStuff}
         </div>
+        <input onChange={e => this.enterCardTitle(e)} value={this.state.newTitle} placeholder='New Card title'></input>
+        <button onClick={() => {
+          this.addCardTitle(this.state.newTitle, this.state.titles)
+        }} >Add New Card</button>
         <h2>{this.state.color}</h2>
         <button onClick={() => this.testFunction()}>Does thing</button>
       </div>
