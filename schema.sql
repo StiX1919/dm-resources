521c6b552a2ddc081f0615fcf06d9ce4bc99af9f
Steven Isbell <steven.isbell18@gmail.com>
2018-04-06 11:29:58 -0500

    ?

--- schema.sql
+++ schema.sql
@@ -1,25 +1,25 @@
 CREATE TABLE general_topic (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
+    general_title VARCHAR(255),
     CONSTRAINT title_unique UNIQUE (title)
 );
 
 CREATE TABLE topic (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
+    topic_title VARCHAR(255),
     general_topic_id INTEGER REFERENCES general_topic (id),
     CONSTRAINT title_unique UNIQUE (title)
 );
 
 CREATE TABLE sub_topic (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
+    sub_title VARCHAR(255),
     topic_id INTEGER REFERENCES topic (id)
 )
 
 CREATE TABLE resource (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
+    resource_title VARCHAR(255),
     description TEXT,
     link VARCHAR(255),
     date_added TIMESTAMP,
