26359d89c5b48bfcf6ae0315dbe5c6492ee6193c
Steven Isbell <steven.isbell18@gmail.com>
2018-04-09 23:08:36 -0500

    updated schema

--- schema.sql
+++ schema.sql
@@ -1,21 +1,20 @@
 CREATE TABLE general_topic (
     id SERIAL PRIMARY KEY,
     general_title VARCHAR(255),
-    CONSTRAINT title_unique UNIQUE (title)
+    CONSTRAINT title_unique UNIQUE (general_title)
 );
 
 CREATE TABLE topic (
     id SERIAL PRIMARY KEY,
     topic_title VARCHAR(255),
     general_topic_id INTEGER REFERENCES general_topic (id),
-    CONSTRAINT title_unique UNIQUE (title)
+    CONSTRAINT topic_title_unique UNIQUE (topic_title)
 );
 
 CREATE TABLE sub_topic (
     id SERIAL PRIMARY KEY,
-    sub_title VARCHAR(255),
-    topic_id INTEGER REFERENCES topic (id)
-)
+    sub_title VARCHAR(255)
+);
 
 CREATE TABLE resource (
     id SERIAL PRIMARY KEY,
@@ -23,8 +22,15 @@ CREATE TABLE resource (
     description TEXT,
     link VARCHAR(255),
     date_added TIMESTAMP,
-    sub_topic_id INTEGER REFERENCES sub_topic (id),
     CONSTRAINT link_unique UNIQUE (link)
-)
+);
+
+CREATE TABLE resource_topic_sub_topic_link (
+    id SERIAL PRIMARY KEY,
+    topic_id INTEGER REFERENCES topic (id),
+    sub_topic_id INTEGER REFERENCES sub_topic (id),
+    resource_id INTEGER REFERENCES resource (id)
+);
+
 
 -- general_topics -> topics -> subtopics -> resources
