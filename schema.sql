5094e73b90dd904d985ddeff6969908555921220
Spencer Smith <stix1919@hotmail.com>
2018-04-11 11:05:34 -0700

    Cleaned up the general topic selection

--- schema.sql
+++ schema.sql
@@ -1,30 +1,36 @@
 CREATE TABLE general_topic (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
-    CONSTRAINT title_unique UNIQUE (title)
+    general_title VARCHAR(255),
+    CONSTRAINT title_unique UNIQUE (general_title)
 );
 
 CREATE TABLE topic (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
+    topic_title VARCHAR(255),
     general_topic_id INTEGER REFERENCES general_topic (id),
-    CONSTRAINT title_unique UNIQUE (title)
+    CONSTRAINT topic_title_unique UNIQUE (topic_title)
 );
 
 CREATE TABLE sub_topic (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
-    topic_id INTEGER REFERENCES topic (id)
-)
+    sub_title VARCHAR(255)
+);
 
 CREATE TABLE resource (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
+    resource_title VARCHAR(255),
     description TEXT,
     link VARCHAR(255),
     date_added TIMESTAMP,
-    sub_topic_id INTEGER REFERENCES sub_topic (id),
     CONSTRAINT link_unique UNIQUE (link)
-)
+);
+
+CREATE TABLE resource_topic_sub_topic_link (
+    id SERIAL PRIMARY KEY,
+    topic_id INTEGER REFERENCES topic (id),
+    sub_topic_id INTEGER REFERENCES sub_topic (id),
+    resource_id INTEGER REFERENCES resource (id)
+);
+
 
 -- general_topics -> topics -> subtopics -> resources
